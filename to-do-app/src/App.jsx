2aab9f1a8ee2adef838e7b4b9754a8149de9fbd6
Will <asampongg377@gmail.com>
2025-01-04 10:46:16 +0000

    Init

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,13 +1,55 @@
-import './App.css'
+import  { useState } from 'react';
+import iconSun from '/images/icon-sun.svg';
 
-function App() {
-  
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const addItem = (event) => {
+    event.preventDefault();
+    if (inputValue.trim() !== '') {
+      setTodoList([...todoList, inputValue]);
+      setInputValue('');
+    }
+  };
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <main className="relative bg-bgDesktop h-screen bg-no-repeat bg-contain w-full bg-VeryDarkBlue">
+      <div className="w-1/4 absolute left-1/2 top-20 -translate-x-1/2">
+        <div className="flex justify-between">
+          <h1 className="text-4xl text-white font-bold tracking-[0.25em]">TODO</h1>
+          <img src={iconSun} alt="Toggle Theme" />
+        </div>
+        <form action="" onSubmit={addItem}>
+          <div className="enter-todo mt-10 shadow-md p-4 bg-VeryDarkDesaturatedBlue flex items-center gap-x-4">
+            <button className="w-5 h-5 border rounded-full" aria-label="Add todo"></button>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              className="w-full bg-transparent outline-none text-white"
+              placeholder="Create a new todo..."
+            />
+          </div>
+        </form>
+        <div className="todo-list bg-VeryDarkDesaturatedBlue py-4 mt-4">
+          {todoList.length === 0 ? (
+            <p className="text-white mt-4">No todos yet. Start by adding one!</p>
+          ) : (
+            <ul className=''>
+              {todoList.map((item, index) => (
+                <li key={index} className="text-white border-b border-gray-500">{item}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
--- to-do-app/tailwind.config.js
+++ to-do-app/tailwind.config.js
@@ -5,7 +5,28 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bgDesktop': "url('/images/bg-desktop-dark.jpg')",
+
+      },
+      colors: {
+        CheckBackground: 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+        // Light theme
+        BrightBlue: 'hsl(220, 98%, 61%)',
+        VeryLightGray: 'hsl(0, 0%, 98%)',
+        VeryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+        LightGrayishBlue: 'hsl(233, 11%, 84%)',
+        DarkGrayishBlue: 'hsl(236, 9%, 61%)',
+        VeryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+        // Dark theme
+        VeryDarkBlue: 'hsl(235, 21%, 11%)',
+        VeryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+        LightGrayishBlue2: 'hsl(234, 39%, 85%)',
+        'LightGrayishBlue(hover)': 'hsl(236, 33%, 92%)',
+        DarkGrayishBlue2: 'hsl(234, 11%, 52%)'
+      }
+    },
   },
   plugins: [],
 }
