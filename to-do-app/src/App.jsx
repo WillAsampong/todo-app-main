c356c7de88000a7bcdb7a56ba5308189a96d3407
Will <asampongg377@gmail.com>
2025-04-04 16:56:56 +0000

    Init

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -1,9 +1,12 @@
 import  { useState } from 'react';
 import iconSun from '/images/icon-sun.svg';
+import iconCheck from '/images/icon-check.svg';
+import iconCross from '/images/icon-cross.svg';
 
 const App = () => {
   const [inputValue, setInputValue] = useState('');
   const [todoList, setTodoList] = useState([]);
+  const [checked, setChecked] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
@@ -17,35 +20,64 @@ const App = () => {
     }
   };
 
+  const deleteItem = (index) => {
+    setTodoList(todoList.filter(( todo) => todo.index !== index))
+  }
+
   return (
-    <main className="relative bg-bgDesktop h-screen bg-no-repeat bg-contain w-full bg-VeryDarkBlue">
-      <div className="w-1/4 absolute left-1/2 top-20 -translate-x-1/2">
-        <div className="flex justify-between">
+    <main className="relative bg-bgDesktop h-screen bg-no-repeat bg-contain w-full">
+      <div className="w-1/3 absolute left-1/2 top-20 -translate-x-1/2 bg-VeryDarkGrayishBlue">
+        <div className="flex items-center justify-between px-2 bg-transparent">
           <h1 className="text-4xl text-white font-bold tracking-[0.25em]">TODO</h1>
-          <img src={iconSun} alt="Toggle Theme" />
+          <img src={iconSun} alt="Toggle Theme" className='w-8 h-8'/>
         </div>
         <form action="" onSubmit={addItem}>
-          <div className="enter-todo mt-10 shadow-md p-4 bg-VeryDarkDesaturatedBlue flex items-center gap-x-4">
-            <button className="w-5 h-5 border rounded-full" aria-label="Add todo"></button>
+          <div className="enter-todo mt-10 shadow-md rounded-sm p-4  flex items-center gap-x-4">
+            <div className="w-5 h-5 border rounded-full" aria-label="Add todo"></div>
             <input
               type="text"
               value={inputValue}
               onChange={handleInputChange}
-              className="w-full bg-transparent outline-none text-white"
+              className="w-full outline-none text-VeryLightGrayishBlue"
               placeholder="Create a new todo..."
             />
           </div>
         </form>
-        <div className="todo-list bg-VeryDarkDesaturatedBlue py-4 mt-4">
+        <div className="todo-list py-2 mt-4">
           {todoList.length === 0 ? (
-            <p className="text-white mt-4">No todos yet. Start by adding one!</p>
+            <p className="text-VeryLightGrayishBlue m-4 text-center">No todos yet. Start by adding one!</p>
           ) : (
             <ul className=''>
               {todoList.map((item, index) => (
-                <li key={index} className="text-white border-b border-gray-500">{item}</li>
+                <li key={index} className="text-VeryLightGrayishBlue border-b border-gray-500 px-4 py-4 flex items-center justify-between w-full">
+                  <div className='flex'>
+                    <button className={`check-button w-5 h-5 rounded-full border ${checked ? "" : ""}`}>
+                      <img src={iconCheck} className=''/>
+                    </button>
+                    <p className='px-4'>{item}</p>
+                  </div>
+                  <button
+                   className='cross-button'
+                   onClick={() => deleteItem(index)}>
+                    <img src={iconCross} className='w-3 cursor-pointer'/>
+                  </button>
+                </li>
               ))}
             </ul>
           )}
+          <div className='flex justify-between p-4'>
+            <div className="num-of-items">
+              <span>{todoList.length} items left</span>
+            </div>
+            <div className="categories flex gap-x-2">
+              <p>All</p>
+              <p>Active</p>
+              <p>Completed</p>
+            </div>
+            <div>
+              <span>Clear Completed</span>
+            </div>
+          </div>
         </div>
       </div>
     </main>
--- to-do-app/tailwind.config.js
+++ to-do-app/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{html,js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
@@ -10,21 +10,22 @@ export default {
         'bgDesktop': "url('/images/bg-desktop-dark.jpg')",
 
       },
+
       colors: {
-        CheckBackground: 'linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)',
+        CheckBackground: 'linear-gradient(hsl(192, 100%, 67%) to hsl(280, 87%, 65%))',
         // Light theme
-        BrightBlue: 'hsl(220, 98%, 61%)',
-        VeryLightGray: 'hsl(0, 0%, 98%)',
-        VeryLightGrayishBlue: 'hsl(236, 33%, 92%)',
-        LightGrayishBlue: 'hsl(233, 11%, 84%)',
-        DarkGrayishBlue: 'hsl(236, 9%, 61%)',
-        VeryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+        'BrightBlue': 'hsl(220, 98%, 61%)',
+        'VeryLightGray': 'hsl(0, 0%, 98%)',
+        'VeryLightGrayishBlue': 'hsl(236, 33%, 92%)',
+        'LightGrayishBlue': 'hsl(233, 11%, 84%)',
+        'DarkGrayishBlue': 'hsl(236, 9%, 61%)',
+        'VeryDarkGrayishBlue': 'hsl(235, 19%, 35%)',
         // Dark theme
-        VeryDarkBlue: 'hsl(235, 21%, 11%)',
-        VeryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
-        LightGrayishBlue2: 'hsl(234, 39%, 85%)',
+        'VeryDarkBlue': 'hsl(235, 21%, 11%)',
+        'VeryDarkDesaturatedBlue': 'hsl(235, 24%, 19%)',
+        'LightGrayishBlue2': 'hsl(234, 39%, 85%)',
         'LightGrayishBlue(hover)': 'hsl(236, 33%, 92%)',
-        DarkGrayishBlue2: 'hsl(234, 11%, 52%)'
+        'DarkGrayishBlue2': 'hsl(234, 11%, 52%)'
       }
     },
   },
